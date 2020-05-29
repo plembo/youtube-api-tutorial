bc2291d094b77d9cbda20042b6b3ff817cd5c610
Phil Lembo <phil.lembo@gmail.com>
2020-05-29 17:12:42 -0400

    First successful request

--- yt.py
+++ yt.py
@@ -1,3 +1,15 @@
 import os
+from googleapiclient.discovery import build
 
 api_key = os.environ.get('YOUTUBE_API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='schafer5'
+)
+
+response = request.execute()
+
+print(response)
