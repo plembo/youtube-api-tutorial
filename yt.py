14a937fa345378eec2687992078135e44e6cfbed
Phil Lembo <phil.lembo@gmail.com>
2021-07-29 21:41:21 -0400

    Use python-dotenv

--- yt.py
+++ yt.py
@@ -1,8 +1,10 @@
 import os
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
 
+load_dotenv()
 api_key = os.environ.get('YOUTUBE_API_KEY')
-
+print(api_key)
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 request = youtube.channels().list(
